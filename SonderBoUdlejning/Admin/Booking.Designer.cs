177776cddf65c79116a11258c404b8f06981c1f5
kaspermark <kaspermarks@gmail.com>
2022-05-15 12:17:02 +0200

    Added combobox

--- SonderBoUdlejning/Admin/Booking.Designer.cs
+++ SonderBoUdlejning/Admin/Booking.Designer.cs
@@ -32,8 +32,17 @@
             this.DGVReservationer = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.CBMembers = new System.Windows.Forms.ComboBox();
+            this.TBPID = new System.Windows.Forms.TextBox();
+            this.TBMail = new System.Windows.Forms.TextBox();
+            this.TBTLF = new System.Windows.Forms.TextBox();
+            this.PersonID = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.PanelPersonInfo = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.DGVRessourcer)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.DGVReservationer)).BeginInit();
+            this.PanelPersonInfo.SuspendLayout();
             this.SuspendLayout();
             // 
             // DGVRessourcer
@@ -73,11 +82,88 @@
             this.label2.Text = "Reservationer";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // CBMembers
+            // 
+            this.CBMembers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CBMembers.FormattingEnabled = true;
+            this.CBMembers.Location = new System.Drawing.Point(413, 263);
+            this.CBMembers.Name = "CBMembers";
+            this.CBMembers.Size = new System.Drawing.Size(121, 21);
+            this.CBMembers.TabIndex = 4;
+            this.CBMembers.SelectedIndexChanged += new System.EventHandler(this.CBMembers_SelectedIndexChanged);
+            // 
+            // TBPID
+            // 
+            this.TBPID.Location = new System.Drawing.Point(8, 22);
+            this.TBPID.Name = "TBPID";
+            this.TBPID.ReadOnly = true;
+            this.TBPID.Size = new System.Drawing.Size(100, 20);
+            this.TBPID.TabIndex = 5;
+            // 
+            // TBMail
+            // 
+            this.TBMail.Location = new System.Drawing.Point(8, 65);
+            this.TBMail.Name = "TBMail";
+            this.TBMail.ReadOnly = true;
+            this.TBMail.Size = new System.Drawing.Size(100, 20);
+            this.TBMail.TabIndex = 6;
+            // 
+            // TBTLF
+            // 
+            this.TBTLF.Location = new System.Drawing.Point(8, 113);
+            this.TBTLF.Name = "TBTLF";
+            this.TBTLF.ReadOnly = true;
+            this.TBTLF.Size = new System.Drawing.Size(100, 20);
+            this.TBTLF.TabIndex = 7;
+            // 
+            // PersonID
+            // 
+            this.PersonID.AutoSize = true;
+            this.PersonID.Location = new System.Drawing.Point(5, 6);
+            this.PersonID.Name = "PersonID";
+            this.PersonID.Size = new System.Drawing.Size(57, 13);
+            this.PersonID.TabIndex = 8;
+            this.PersonID.Text = "Person ID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Mail:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Telefonnummer: ";
+            // 
+            // PanelPersonInfo
+            // 
+            this.PanelPersonInfo.Controls.Add(this.label5);
+            this.PanelPersonInfo.Controls.Add(this.label4);
+            this.PanelPersonInfo.Controls.Add(this.PersonID);
+            this.PanelPersonInfo.Controls.Add(this.TBTLF);
+            this.PanelPersonInfo.Controls.Add(this.TBMail);
+            this.PanelPersonInfo.Controls.Add(this.TBPID);
+            this.PanelPersonInfo.Location = new System.Drawing.Point(562, 241);
+            this.PanelPersonInfo.Name = "PanelPersonInfo";
+            this.PanelPersonInfo.Size = new System.Drawing.Size(131, 150);
+            this.PanelPersonInfo.TabIndex = 11;
+            this.PanelPersonInfo.Visible = false;
+            // 
             // Booking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1181, 450);
+            this.Controls.Add(this.PanelPersonInfo);
+            this.Controls.Add(this.CBMembers);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.DGVReservationer);
@@ -87,6 +173,8 @@
             this.Load += new System.EventHandler(this.Booking_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGVRessourcer)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DGVReservationer)).EndInit();
+            this.PanelPersonInfo.ResumeLayout(false);
+            this.PanelPersonInfo.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +186,13 @@
         private System.Windows.Forms.DataGridView DGVReservationer;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CBMembers;
+        private System.Windows.Forms.TextBox TBPID;
+        private System.Windows.Forms.TextBox TBMail;
+        private System.Windows.Forms.TextBox TBTLF;
+        private System.Windows.Forms.Label PersonID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel PanelPersonInfo;
     }
 }
--- SonderBoUdlejning/Admin/Booking.cs
+++ SonderBoUdlejning/Admin/Booking.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace SonderBoUdlejning.Admin
 {
     public partial class Booking : Form
     {
+        ConnString connString = ConnString.getConnInstance;
         SQLExecutionHandler tableConn = new SQLExecutionHandler();
         string sqlS1 = "SELECT resNr, rTypeNavn, pId, Ressourcer.rId, rType, rStartDato, rSlutDato FROM Ressourcer INNER JOIN Reservationer ON Ressourcer.rId = Reservationer.rId";
         string sqlS2 = "SELECT*FROM Ressourcer WHERE rId NOT IN(SELECT rId FROM Reservationer)";    
@@ -25,13 +27,50 @@ namespace SonderBoUdlejning.Admin
         {
             DGVReservationer.DataSource = tableConn.tableBinder(sqlS1);
             DGVRessourcer.DataSource = tableConn.tableBinder(sqlS2);
+
+            List<string> listBeboer = new List<string>();
+
+            string query = "SELECT COUNT(fNavn) FROM Person WHERE erBeboer = 1";
+            SqlConnection conn = new SqlConnection(connString.connStr);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            int antalBeboer = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+
+            string query2 = "SELECT fNavn FROM Person WHERE erBeboer = 1";
+            SqlConnection conn2 = new SqlConnection(connString.connStr);
+            SqlCommand cmd2 = new SqlCommand(query2, conn2);
+            
+            conn2.Open();
+            SqlDataReader reader = cmd2.ExecuteReader();
+            for (int i = 0; i < antalBeboer; i++) 
+            {
+                while (reader.Read()) 
+                {
+                    listBeboer.Add(reader.GetString(i));
+                }
+            }
+            conn2.Close();
+
+
+            foreach (string item in listBeboer) 
+            {
+                CBMembers.Items.Add(item);
+            }
         }
+
+        
 
         //Tag inspiration fra vAddToList linje 17
 
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void CBMembers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PanelPersonInfo.Visible = true;
         }
     }
 }
