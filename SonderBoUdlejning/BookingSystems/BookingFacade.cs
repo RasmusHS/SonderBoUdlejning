c8ce34b4fa96e0cd0eb6159337aef00e53fca663
Rasmus Høy Schmidt <rasshoey@gmail.com>
2022-05-25 10:57:31 +0200

    Lavede facade til booking og flyttede CreateDir.cs ud af dens mappe

---
+++ SonderBoUdlejning/BookingSystems/BookingFacade.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace SonderBoUdlejning.BookingSystems
+{
+    public static class BookingFacade
+    {
+        public static int mellemDatoer;
+        public static void checkMellemDatoer(string startDato, string slutDato, int rid)
+        {
+            //CheckMellemDatoer checkMellem = new CheckMellemDatoer();
+            mellemDatoer = CheckMellemDatoer.CheckDatesForBookings(startDato, slutDato, rid);
+        }
+
+        public static int ressourceIdType;
+        public static void checkRessourceIdType(string tbResourceID)
+        {
+            //CheckResourceIdType checkResourceId = new CheckResourceIdType();
+            ressourceIdType = CheckResourceIdType.getResourceIdType(tbResourceID);
+        }
+
+        public static int slutDato;
+        public static void checkSlutDato(string slutdato, string resourceID)
+        {
+            //CheckSlutDato checkSlut = new CheckSlutDato();
+            slutDato = CheckSlutDato.CheckSlutDate(slutdato, resourceID);
+        }
+
+        public static void getPersonList(List<int> beboerID, List<string> beboer)
+        {
+            //OnLoadFunctions getPerson = new OnLoadFunctions();
+            OnLoadFunctions.GetPersonList(beboerID, beboer);
+        }
+
+        public static void getRessourceList(List<int> listResourceID, List<string> listResource, string date)
+        {
+            //OnLoadFunctions getRessource = new OnLoadFunctions();
+            OnLoadFunctions.GetResourceList(listResourceID, listResource, date);
+        }
+           
+        public static void setArrayInfo(string[] rTypeNavnArray, string[] antalReservationerArray, SqlConnection conn)
+        {
+            //SetArrayInfo setArrayInfo = new SetArrayInfo();
+            SetArrayInfo.FillArrays(rTypeNavnArray, antalReservationerArray, conn);
+        }
+    }
+}
---
+++ SonderBoUdlejning/CreateDir.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SonderBoUdlejning
+{
+    internal class CreateDir
+    {
+        public static void CreateDirectory() 
+        {
+            string username = Environment.UserName;
+            string dir1 = $@"C:\Users\{username}\Documents\SønderBoUdlejning\Statistik";
+            string dir2 = $@"C:\Users\{username}\Documents\SønderBoUdlejning\Kontrakter\Aktive_Lejekontrakter";
+            // If directory does not exist, create it
+            if (!Directory.Exists(dir1))
+            {
+                Directory.CreateDirectory(dir1);
+            }
+
+            if (!Directory.Exists(dir2))
+            {
+                Directory.CreateDirectory(dir2);
+            }
+        }
+    }
+}
